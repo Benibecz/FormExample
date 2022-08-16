fdff8e7c72b898d4ccef40f8c02dc1682958329c
Benito Beceiro <benito_bec@hotmail.com>
2022-08-16 16:25:40 +0100

    updated js and responsive

--- script.js
+++ script.js
@@ -2,51 +2,59 @@
 
 const inputEmail = document.querySelector("#form__email--address");
 const inputName = document.querySelector("#form__name");
-const inputComment = document.querySelector("#form__comment");
-const errorMessage = document.querySelector(".error__message");
-const formElement = document.querySelector("form");
+const inputText = document.querySelector("#form__comment");
 const btnSubmit = document.querySelector("#btn__submit");
-const successfullMessage = document.querySelector(".successfull__message");
-const bodyElement = document.querySelector("body");
+const errorMessage = document.querySelector(".error__message");
+const successfulMessage = document.querySelector(".successfull__message");
+const formElement = document.querySelector("form");
+const checkBox = document.querySelector("#form__terms--conditions");
 
-const clearInputs = () => {
-  inputEmail.value = "";
-  inputName.value = "";
-  inputComment.value = "";
+const clearInputs = function () {
+  inputEmail.textContent = "";
+  inputName.textContent = "";
+  inputText.textContent = "";
 };
 
-const errorFunction = () => {
+const errorFunction = function () {
   errorMessage.classList.remove("hidden");
-  errorMessage.innerHTML = "";
+  errorMessage.textContent = "";
 };
 
-formElement.addEventListener("submit", function (e) {
+const messageFunction = function (a, b) {
+  return `${a} must be ${b} characters`;
+};
+
+btnSubmit.addEventListener("click", function (e) {
   e.preventDefault();
+  console.log(inputText.value.replaceAll("/", ""));
 
   if (!inputEmail.value.includes("@")) {
     errorFunction();
-    errorMessage.innerHTML = "Please enter a valid email address";
-    clearInputs();
-  } else if (inputName.value.length <= 2 || inputName.value.length >= 50) {
+    errorMessage.textContent = "Please enter a valid email address";
+  } else if (
+    (inputName.value.length > 0 && inputName.value.length < 2) ||
+    inputName.value.length >= 50
+  ) {
+    const type =
+      (inputName.value.length < 2 && "at least 2") ||
+      (inputName.value.length >= 50 && "no longer than 50");
     errorFunction();
-    errorMessage.innerHTML = "Please enter a valid username";
-    clearInputs();
-  } else if (inputComment.value.length <= 50) {
+    errorMessage.textContent = messageFunction("username", type);
+  } else if (inputText.value.length <= 50 || inputText.value.length >= 280) {
+    const type =
+      (inputText.value.length <= 50 && "at least 50") ||
+      (inputText.value.length >= 280 && "less than 280");
     errorFunction();
-    errorMessage.innerHTML =
-      "Comment text must not be less than 50 characters ";
-    clearInputs();
-  } else if (inputComment.value.length >= 280) {
+    errorMessage.textContent = messageFunction("comment", type);
+  } else if (inputText.value.includes("/")) {
     errorFunction();
-    errorMessage.innerHTML =
-      "Comment text must not be longer than 280 characters";
-    clearInputs();
+    errorMessage.textContent = "Comment must not contain links";
+  } else if (checkBox.checked === false) {
+    errorFunction();
+    errorMessage.textContent = "Terms & Conditions tick-box must be ticked";
   } else {
-    errorMessage.innerHTML = "";
     formElement.classList.add("hidden");
-    successfullMessage.classList.remove("hidden");
-    successfullMessage.classList.add("flex");
-
-    clearInputs();
+    successfulMessage.classList.remove("hidden");
+    successfulMessage.classList.add("flex");
   }
 });
